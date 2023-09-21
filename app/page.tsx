c7fdaa22b6a2a6906185caec162e2e13baafa659
Erin <eplewis89@gmail.com>
2023-09-20 22:31:34 -0700

    nextjs now

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="flex justify-center items-center">
+        <img src="/graveseer-logo-dead.png"/>
+      </div>
+      <div className="flex justify-center items-center">
+        <p>site coming soon...</p>
+      </div>
+    </main>
+  )
+}
