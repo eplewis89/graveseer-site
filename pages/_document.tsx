95585a2c1d2706a0f8a47194785e62280ca0bead
Erin <eplewis89@gmail.com>
2023-12-14 11:02:43 -0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import { Analytics } from '@vercel/analytics/react';
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -7,6 +9,7 @@ export default function Document() {
       <body className="bg-main bg-opacity-50 bg-no-repeat bg-cover bg-center bg-fixed">
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   )
