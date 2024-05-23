b124d181d2544273601e8026d6891581f7de7b75
Erin <eplewis89@gmail.com>
2024-05-23 11:32:42 -0700

    fixing background

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,7 +8,7 @@ function Layout({ children }: any) {
     return (
         <div className="backdrop-blur-[2px] backdrop-brightness-50 min-h-screen">
             <Header />
-            <article className="flex flex-col items-center align-center mx-auto md:w-full lg:w-1/3">
+            <article className="flex flex-col items-center align-center mx-auto md:w-full bg-black/75">
                 {children}
             </article>
             <Footer />
