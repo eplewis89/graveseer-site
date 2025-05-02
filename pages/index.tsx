76b9d9cd291aebac771b61b3d995a152fdd75f04
Erin <eplewis89@gmail.com>
2025-05-01 19:38:46 -0700

    add space bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,6 +41,18 @@ const Page = () => {
         <div className="text-center text-md m-4">
           <p className="p-2">
             <Image 
+              src="/2025-may-6-spacebar.jpg" 
+              alt="graveseer space bar may 6 2025"
+              width={800}
+              height={500}/>
+          </p>
+        </div>
+        <div className="text-center text-md m-4 invisible">
+          <p className="p-2">-</p>
+        </div>
+        <div className="text-center text-md m-4">
+          <p className="p-2">
+            <Image 
               src="/2025-jul-6-humbleheart.jpg" 
               alt="graveseer humble heart jul 6 2025"
               width={800}
